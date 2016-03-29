9294214d52be790b924ca7f1cf35fb462338276f
Tony Thompson <ajthomps51@gmail.com>
2016-03-29 20:19:54 +0200

    made htag labels less ambiguous

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@
 
 [[ -z "$1" ]] && echo Please two htags as an argument! E.G source update.sh h011 h010 && return
 [[ -z "$2" ]] && echo Please another htag as an argument! E.G source update.sh h011 h010 && return
+source setup.sh
 # May need to change these in the future
 export datasetDir=/eos/atlas/atlascerngroupdisk/phys-higgs/HSG1/MxAOD
 export MXAODTYPES=(MxAOD data AllSys PhotonSys)
@@ -12,47 +13,47 @@ export mcDir=mc_25ns
 export dataDir=data_25ns
 export AllSysDir=AllSys 
 export PhotonSysDir=PhotonSys 
-htag=$1
+htagNew=$1
 htagOld=$2
-echo Updating for new htag: $htag
+echo Updating for new htag: $htagNew
 echo Comparing to old htag: $htagOld
 
 
-echo $htag > CurrentHtag.txt
+echo $htagNew > CurrentHtag.txt
 
 currentDir=$(pwd)
 echo Updating Front Page Cutflows...
 cd cutflows
-source getCutflows.sh $htag
+source getCutflows.sh $htagNew
 cd ..
 
 echo Updating Variable Lists...
 cd variables
-source getVarDiffs.sh $htag $htagOld
-source getFullVarList.sh $htag
+source getVarDiffs.sh $htagNew $htagOld
+source getFullVarList.sh $htagNew
 cd ..
 
 echo Updating File sizes...
 cd fileSize
-source getFileSize.sh $htag
+source getFileSize.sh $htagNew
 cd ..
 
 echo updating ALL cutflows
 cd AllCutflows
-source getCutflows.sh $htag $htagOld
+source getCutflows.sh $htagNew $htagOld
 cd ..
 
 echo Making webpages for ALL samples...
-source makePages $htag $htagOld
+source makePages $htagNew $htagOld
 
-echo Updating live search for $htag
+echo Updating live search for $htagNew
 cd liveSearch
-source makeXMLforLiveSearch.sh $htag
+source makeXMLforLiveSearch.sh $htagNew
 cd ..
 
 echo updating ALL plots.  This will take a long time...
 cd plotter
-source makePlots.sh $htag $htagOld
+source makePlots.sh $htagNew $htagOld
 cd ..
 
 
