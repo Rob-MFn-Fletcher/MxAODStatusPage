510de2ed5bc67cbe9f9b83ec456e5c16d953a04f
Tony Thompson <ajthomps51@gmail.com>
2016-04-04 20:18:12 +0200

    formatting

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ export MXAOD_MC_TYPES=(MxAOD MxAODAllSys MxAODPhotonSys)              # types of
 export MXAOD_MC_DIRS=(mc_25ns AllSys PhotonSys)                       # folders of MC MxAODs on EOS 
 export MXAODTYPES=(MxAOD MxAODAllSys MxAODPhotonSys)                  # types of MxAODs (including MC and data) 
 export MXAODDIRS=(mc_25ns data_25ns AllSys PhotonSys)                 # folders of MxAODs (including MC and data)
+export mcDir=mc_25ns                                                  # regular MC directory
+export dataDir=data_25ns                                              # data directory
 export DATANAME=physics_Main                                          # unique identifier for data files
 export COLLECTION_TREE_NAME="CollectionTree"                          # tree where kinematic variables are located in MxAOD
 export EXAMPLEFILE=PowhegPy8_VBF125_small                             # file used for variable dumps/comparisons and file size
@@ -15,13 +17,10 @@ export EXAMPLEFILE=PowhegPy8_VBF125_small                             # file use
 # Variables to be plotted
 export VARSFORPLOTS=(HGamEventInfoAuxDyn.cosTS_yy HGamEventInfoAuxDyn.cosTS_yyjj HGamEventInfoAuxDyn.Dphi_yy_jj HGamEventInfoAuxDyn.DRmin_y_j HGamEventInfoAuxDyn.DR_y_y HGamEventInfoAuxDyn.Dy_j_j HGamEventInfoAuxDyn.Dy_y_y HGamEventInfoAuxDyn.Dy_yy_jj HGamEventInfoAuxDyn.E_y1 HGamEventInfoAuxDyn.E_y2 HGamEventInfoAuxDyn.m_ee HGamEventInfoAuxDyn.met_TST HGamEventInfoAuxDyn.m_jj HGamEventInfoAuxDyn.m_mumu HGamEventInfoAuxDyn.mu HGamEventInfoAuxDyn.m_yy_hardestVertex HGamEventInfoAuxDyn.m_yy HGamEventInfoAuxDyn.m_yy_resolution HGamEventInfoAuxDyn.m_yy_zCommon HGamEventInfoAuxDyn.N_e HGamEventInfoAuxDyn.N_j HGamEventInfoAuxDyn.NLoosePhotons HGamEventInfoAuxDyn.N_mu HGamEventInfoAuxDyn.numberOfPrimaryVertices HGamEventInfoAuxDyn.phi_TST HGamEventInfoAuxDyn.pT_hard HGamEventInfoAuxDyn.pT_j1 HGamEventInfoAuxDyn.pT_j2 HGamEventInfoAuxDyn.pT_jj HGamEventInfoAuxDyn.pTt_yy HGamEventInfoAuxDyn.pT_y1 HGamEventInfoAuxDyn.pT_y2 HGamEventInfoAuxDyn.pT_yy HGamEventInfoAuxDyn.sumet_TST HGamEventInfoAuxDyn.yAbs_yy HGamEventInfoAuxDyn.Zepp)
 
-
 # Variables for outputing the cutflows.  Only ALLEVTS is used in the code, code is copied from HGamTools
 export VARSFORCUTFLOWS=("NxAOD=0" "NDxAOD=1" "ALLEVTS=2" "DUPLICATE=3" "TRIGGER=4" "GRL=5" "DQ=6" "VERTEX=7" "TWO_LOOSE_GAM=8" "AMBIGUITY=9" "TRIG_MATCH=10" "GAM_TIGHTID=11" "GAM_ISOLATION=12" "RELPTCUTS=13" "MASSCUT=14" "PASSALL=15")
 
 
-export mcDir=mc_25ns
-export dataDir=data_25ns
 [[ ! -d "$EOSMOUNTDIR/$datasetDir" ]] && output=$(eosmount eos 2>&1)
 if [[ ! -d "$EOSMOUNTDIR/$datasetDir" ]]; then
   echo $output
