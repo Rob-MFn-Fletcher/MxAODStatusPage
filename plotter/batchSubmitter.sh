5dbad2bf4fb48e981121d5a48c93a74c9bb3c1c0
Tony Thompson <ajthomps51@gmail.com>
2016-04-06 22:12:16 +0200

    added some comments

--- plotter/batchSubmitter.sh
+++ plotter/batchSubmitter.sh
@@ -22,6 +22,7 @@ logFileSize=$(ls -l $BASEDIR/plotter/submit.out | awk '{print $5}')
 echo submitting ${#Samples[@]} lxplus batch jobs for plots...
 progress=$(( 0 ))
 barWidth=$(( 70 ))
+rm -r $BASEDIR/plotter/outputbatch/*
 for fileName in ${Samples[@]}; do
   pos=$(echo "scale=0; $barWidth * $progress" | bc)
   posRound=$(echo "($pos + 0.5) / 1" | bc)
@@ -37,6 +38,7 @@ for fileName in ${Samples[@]}; do
   done
   progressRound=$(echo "($progress  * 100.0 + 0.5)/1" | bc)
   echo -ne '] '$(echo "scale=0; $progressRound" | bc) " %\r"
+  # generously submitted to the 1nd queue (cput time 1 day), could split up by time, size, etc. Most are finished in ~15 minutes
   bsub -q 1nd $BASEDIR/plotter/makePlotBatch.sh $fileName $htagNew $htagOld >> $BASEDIR/plotter/submit.out
   progress=$(echo "scale=5; $progress + 1.0/${#Samples[@]}" | bc)
 done
