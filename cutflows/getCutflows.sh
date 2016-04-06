abff8f78459a39ff70ab1268fca0eb316c694e93
Tony Thompson <ajthomps51@gmail.com>
2016-04-06 21:58:31 +0200

    supressed more root errors

--- cutflows/getCutflows.sh
+++ cutflows/getCutflows.sh
@@ -27,7 +27,7 @@ file="root://eosatlas.cern.ch/$datasetDir/$htag/$dataDir/$fileName"
 echo $file
 base=$(basename ${file})
 fileType=${fileName%.MxAOD*}
-root -l -q -b "printCutflowData.c(\"${file}\")" > ${fileType}.txt
+root -l -q -b "printCutflowData.c(\"${file}\")" 2>err.log  1> ${fileType}.txt
 sed -i'.og' "1d" ${fileType}.txt
 sed -i'.og' "s/^ *//g" ${fileType}.txt
 sed -i'.og' "s/Processing.*/                    $fileType/g" ${fileType}.txt
