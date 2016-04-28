9cff07733f66f891b19ddb03fc1ec83f68448639
Tony Thompson <ajthomps51@gmail.com>
2016-04-28 20:02:37 +0200

    added progress bar, nicer output

--- cutflows/getCutflows.sh
+++ cutflows/getCutflows.sh
@@ -1,26 +1,26 @@
 # dumps the cutflows of the mH=125 MxAODs to txt files, which are read by index.php
 [[ -z "$1" ]] && echo Please enter an htag as an argument! E.G source getCutflows.sh h011 && return
-#datasetDir=/Users/athompso/MxAODwebsite/datasets
 htag=$1
-#datasetDir=/eos/atlas/atlascerngroupdisk/phys-higgs/HSG1/MxAOD
-#mcDir=mc_25ns
-#dataDir=data_25ns
 Samples=($(eos ls $datasetDir/$htag/$mcDir/ | grep 125))
 
 
 # mc
+echo '    updating nominal MC samples (mH=125)...'
+resetProgressBar
 for fileName in ${Samples[@]}; do
     file="root://eosatlas.cern.ch/$datasetDir/$htag/$mcDir/$fileName"
-    echo $file
+    #echo $file
     base=$(basename ${file})
     fileType=${fileName%.MxAOD*}
     root -l -q -b "printCutflow.c(\"${file}\")" 2>err.log  1> ${fileType}.txt
     sed -i'.og' "1d" ${fileType}.txt
     sed -i'.og' "s/^ *//g" ${fileType}.txt
     sed -i'.og' "s/Processing.*/                    $fileType/g" ${fileType}.txt
+    tickProgressBar ${#Samples[@]}
 done
-
+endProgressBar
 # All Data
+echo '    updating data cutflow...'
 
 fileName=$(eos ls $datasetDir/$htag/$dataDir/ | grep periodAll25ns)
 file="root://eosatlas.cern.ch/$datasetDir/$htag/$dataDir/$fileName"
