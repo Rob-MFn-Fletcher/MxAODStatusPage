ce56d190c5ad3eaed777ad03aa1d21d77382c2bf
Tony Thompson <ajthomps51@gmail.com>
2016-04-28 16:13:18 +0200

    moved status bar to setup script

--- plotter/batchSubmitter.sh
+++ plotter/batchSubmitter.sh
@@ -10,36 +10,28 @@ sed -i "s|^export BASEDIR=.*|export BASEDIR=${BASEDIR}|g" $BASEDIR/plotter/makeP
   varsStringArray=$(echo ${VARSFORPLOTS[@]} | sed "s/^/std::string___plotVars[${#VARSFORPLOTS[@]}]={\"/g" | sed 's/$/"};/g' | sed 's/ /","/g' | sed 's/___/ /g')
 echo $varsStringArray > $BASEDIR/plotter/Root/plotVars.h
 echo '// This file is produced automatically in makePlots.sh. To change the variables that are plotted, change the list in the variable VARSFORPLOTS in the setup script' >> $BASEDIR/plotter/Root/plotVars.h
+
 Samples=()
 for DIR in ${MXAODDIRS[@]}; do
   Samples+=($(eos ls $datasetDir/$htagNew/$DIR/ ))
-  #break
 done
 
 logFileSize=$(ls -l $BASEDIR/plotter/submit.out | awk '{print $5}')
 [[ ! -z $logFileSize ]] && [[ $logFileSize -gt 1000000  ]] && rm $BASEDIR/plotter/submit.out
 
+fileNew=""
+sampleDir=""
+for DIR in ${MXAODDIRS[@]}; do
+  [[ ! -z "$(eos ls $datasetDir/$htagNew/$DIR/$fileName 2>/dev/null)"  ]] && fileNew="$EOSMOUNTDIR/$datasetDir/$htagNew/$DIR/$fileName" && sampleDir=$DIR
+done
+
 echo submitting ${#Samples[@]} lxplus batch jobs for plots...
-progress=$(( 0 ))
-barWidth=$(( 70 ))
 rm -r $BASEDIR/plotter/outputbatch/*
+resetProgressBar
+
 for fileName in ${Samples[@]}; do
-  pos=$(echo "scale=0; $barWidth * $progress" | bc)
-  posRound=$(echo "($pos + 0.5) / 1" | bc)
-  echo -ne '['
-  for i in $(seq 0 $barWidth); do
-    if   [[ "$i" -lt "$posRound" ]]; then
-      echo -ne '='
-    elif [[ "$i" -eq "$posRound" ]]; then
-      echo -ne '>'
-    else
-      echo -ne ' '
-    fi
-  done
-  progressRound=$(echo "($progress  * 100.0 + 0.5)/1" | bc)
-  echo -ne '] '$(echo "scale=0; $progressRound" | bc) " %\r"
   # generously submitted to the 1nd queue (cput time 1 day), could split up by time, size, etc. Most are finished in ~15 minutes
   bsub -q 1nd $BASEDIR/plotter/makePlotBatch.sh $fileName $htagNew $htagOld >> $BASEDIR/plotter/submit.out
-  progress=$(echo "scale=5; $progress + 1.0/${#Samples[@]}" | bc)
+  tickProgressBar ${#Samples[@]}
 done
-echo
+endProgressBar
