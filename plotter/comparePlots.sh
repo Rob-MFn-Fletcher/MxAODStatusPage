884e54f4e074fba2e0489fc0eaaa2c3518e7e516
Tony Thompson <ajthomps51@gmail.com>
2016-05-27 18:03:48 +0200

    change to use PHP base variable

--- plotter/comparePlots.sh
+++ plotter/comparePlots.sh
@@ -1,11 +1,12 @@
-BASEDIR=/afs/cern.ch/user/a/athompso/www
+#BASEDIR=/afs/cern.ch/user/a/athompso/www
 source /afs/cern.ch/sw/lcg/app/releases/ROOT-externals/ROOT-latest/x86_64-slc6-gcc48-opt/setup.sh
 source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.06.04/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
 export DISPLAY=localhost:0.0
 #root -l -b -q '/afs/cern.ch/user/a/athompso/www/plotter/Root/plotCompare.c("/afs/cern.ch/user/a/athompso/www/plotter/samples/h011/Pythia8_WH80.MxAOD.p2421.h011.root","h011","/afs/cern.ch/user/a/athompso/www/plotter/samples/h010/Pythia8_WH80.MxAOD.p2421.h010.root","h010")'
 
-currSample=$1
-compSample=$2
+BASEDIR=$1
+currSample=$2
+compSample=$3
 compHtag=$(echo $compSample | grep -o '\.h[0-9][0-9][0-9].*\.' ) # make sure I don't get something stupid like ggh125
 compHtag=${compHtag%?}
 compHtag=${compHtag#?}
