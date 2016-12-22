7bbc5d2e967792af656a865584cb8729817ae032
Rob Fletcher <robroy.fletcher@gmail.com>
2016-12-22 16:16:09 -0500

    Backing up work.

--- dataValidation/NeventsCheck/checkEvents.py
+++ dataValidation/NeventsCheck/checkEvents.py
@@ -1,3 +1,6 @@
+"""Check events numbers from AODs and DAODs from AMI and the files themselves.
+
+"""
 import argparse
 from ROOT import TFile,TH1
 import pyAMI.client
@@ -8,22 +11,75 @@ import os
 import re
 from glob import glob
 
-def getAMIProv():
+# Setup a few things that we only want to initialize once.
+client = pyAMI.client.Client('atlas')
+AtlasAPI.init()
+
+def getAMIProv(sample):
     """This function includes all AMI interface components
 
     This function uses the pyAMI API to get all needed information
     from the database and returns the event counts in a dictionary.
+    The provenance is all files that the dataset were produced from, or that
+    were used to produce the dataset. The 'distance' determines how many steps
+    away from the dataset the entry is. e.g. -1 is the file that was used to
+    produce the dataset, and 1 is a file that was produced FROM the dataset.
+
+    --sample should be the full dataset name as you would search for it in AMI
+      with *NO* trailing /.
     """
-    amiEvents = None
+    amiEvents = {}
+    # Get the provenance from AMI
+    sample.rstrip('/') # 
+    prov = AtlasAPI.get_dataset_prov(client, sample)
+    # Loop over all of the datasets in the provenance
+    for dataset in prov['node']:
+        # Get nevents from the AOD that this sample was produced from
+        if dataset['dataType'] == 'AOD':
+            if dataset['distance'] == -1: # The file imediately before the derivation
+                amiEvents['AOD_AMI'] = dataset['events']
+        # Get the nEvents from the derivation sample.
+        if dataset['dataType'] == 'DAOD_HIGG1D1':
+            if dataset['distance'] == 0: # The actual sample. Any higher numbers are files made from this one.
+                amiEvents['DAOD_AMI'] = dataset['events']
+
     return amiEvents
 
-def getROOTInfo():
+def getROOTInfo(sample):
     """Get event number from the root files on eos.
 
     Get the root file and read all of the event number information from it.
+    For MC samples this will just be one cutflow histo. For data there could be
+    one histo for each run in the combined files. These need to have the numbers
+    added for each one of the runs.
     Return as a dictionary of results.
     """
-    rootEvents = None
+    # setup the output dictionary
+    rootEvents = {}
+    rootEvents['AOD_Bookkeeper'] = 0
+    rootEvents['DxAOD_Bookkeeper'] = 0
+    rootEvents['NevtsRunOverMxAOD'] = 0
+    rootEvents['NevtsPassedPreCutflowMxAOD'] = 0  # Ambiguity cut in the cutflow
+
+    rfile = TFile(sample)
+    # loop over everything in the file and get cutflow hitos
+    for key in rfile.GetListOfKeys():
+        name = key.GetName()
+        if not "CutFlow" in name: continue #skip things that are not a cutflow histo
+        if ("Dalitz" in name or "weighted" in name): continue #skip the Dalitz and weighted histos
+        hist = key.ReadObj()
+        # Get events from the Cutflow histogram and add them to the totals.
+        rootEvents['AOD_Bookkeeper'] += hist.GetBinContent(1)
+        rootEvents['DxAOD_Bookkeeper'] += hist.GetBinContent(2)
+        rootEvents['NevtsRunOverMxAOD'] += hist.GetBinContent(3)
+        # Preselection events from the cutflow histogram.
+        rootEvents['NevtsPassedPreCutflowMxAOD'] += hist.GetBinContent(10) # Ambiguity cut in the cutflow
+
+    # Now get the preselection events from the flags in the tree.
+    tree = rfile.Get("CollectionTree")
+    nEvtsPre = tree.GetEntries("HGamEventInfoAuxDyn.isPassedBasic && HGamEventInfoAuxDyn.isPassedPreselection")
+    rootEvents['NevtsIsPassedPreFlagMxAOD'] = nEvtsPre
+
     return rootEvents
 
 def validColor(combInfo):
@@ -34,7 +90,7 @@ def validColor(combInfo):
     by the result of the comparison of event numbers between AMI and ROOT files.
 
     """
-    colorCode = "red"
+    colorCode = ""
     return colorCode
 
 def readInputFile(inFile):
@@ -73,6 +129,9 @@ def runMC(args):
 
     if agrs.v: print "Out directory:",outHtagDir
 
+    # Get the dictionary made from the input file
+    inputMC = readInputFile(args.inputMC)
+
     # Get a list of all directories that start with mc. This should only return one.
     # NOTE: glob returns full file paths!! e.g. for h014 this command returns:  ['./eos/atlas/atlascerngroupdisk/phys-higgs/HSG1/MxAOD/h014/mc15c/']
     mxoadSamplesDir = glob(args.datasetDir+args.htag+'/mc*/')
@@ -89,29 +148,45 @@ def runMC(args):
     mxaod_multi_samples = glob(mxaodSamplesDir[0]+'*/')
 
     #loop over samples and get information
-    jsonMCOutput = []
+    jsonOutput = []
     for sample in mxaodSamples:
         # Deal with the dirs of root files later. Skip for now.
         if sample in mxaod_multi_samples:
             continue
 
         rootInfo = getROOTInfo(sample)
-        amiInfo = getAMIProv(sample)
+
+        # get the sampleType from the path. e.g. /path/to/mc15a.Sherpa_ADDyy_MS3500_1800M.MxAOD.p2610.h013x.root
+        # will return 'Sherpa_ADDyy_MS3500_1800M'
+        sampleType = re.search('mc.*\.(.*)\.MxAOD.*').group(1)
+        amiInfo = getAMIProv(inputMC[sampleType]) #needs AMI dataset name as input
 
         # Combine these dictionaries into a single dictionary to write out.
         combInfo = dict(rootInfo.items() + amiInfo.items())
 
+        # If there are mismatches in the nEvents set the color to red. White otherwise.
         color = validColor(combInfo)
         combInfo["color"] = color
 
         # Append the dictionary to a list of samples
-        jsonMCSamplesOutput.append(combInfo)
+        jsonOutput.append(combInfo)
     pass ## end sample loop
 
     #Output to file for use on website.
-    json.dump(jsonMCSamplesOutput, "../data/%s/ValidationTable_MC.txt".format(args.htag))
+    with open("../data/%s/ValidationTable_MC.txt".format(args.htag)) as outfile:
+        json.dump(jsonOutput, outfile, indent=2)
 
     ### End runMC() function
+
+def runData(args):
+    """Run over the data samples.
+
+    For data we need to look at files inside of the folder /data15, /data16, and also the
+    /runs folder inside each of those. This is differnt than what MC looks like so
+    we need a different function. For the most part this is the same as the runMC() function.
+    """
+    pass
+
 
 def validHTag(htag):
     """This is used in an argparse type to ensure that the htag argument is in the proper format.
