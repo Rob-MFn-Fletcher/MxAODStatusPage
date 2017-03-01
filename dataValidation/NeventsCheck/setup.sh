ff68f64433e043ba809b273dc6da28b16a670c58
Rob-MFn-Fletcher <robroy.fletcher@gmail.com>
2017-03-01 17:51:00 -0400

    All sys files added to setup

--- dataValidation/NeventsCheck/setup.sh
+++ dataValidation/NeventsCheck/setup.sh
@@ -8,4 +8,7 @@ eosmount ./eos
 svn export svn+ssh://rfletche@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/HiggsPhys/Run2/HGamma/xAOD/HGamTools/branches/HGamTools-00-00-51-branch/data/input/data1516_13TeV.txt ./InputFiles/data_h015.txt
 svn export svn+ssh://rfletche@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/HiggsPhys/Run2/HGamma/xAOD/HGamTools/branches/HGamTools-00-00-51-branch/data/input/mc15_13TeV.txt ./InputFiles/mc_h015.txt
 svn export svn+ssh://rfletche@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/HiggsPhys/Run2/HGamma/xAOD/HGamTools/branches/HGamTools-00-00-51-branch/data/input/PhotonSys.txt ./InputFiles/PhotonSys_h015.txt
-
+svn export svn+ssh://rfletche@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/HiggsPhys/Run2/HGamma/xAOD/HGamTools/branches/HGamTools-00-00-51-branch/data/input/PhotonAllSys.txt ./InputFiles/PhotonAllSys_h015.txt
+svn export svn+ssh://rfletche@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/HiggsPhys/Run2/HGamma/xAOD/HGamTools/branches/HGamTools-00-00-51-branch/data/input/LeptonMETSys.txt ./InputFiles/LeptonMETSys_h015.txt
+svn export svn+ssh://rfletche@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/HiggsPhys/Run2/HGamma/xAOD/HGamTools/branches/HGamTools-00-00-51-branch/data/input/JetSys.txt ./InputFiles/JetSys_h015.txt
+svn export svn+ssh://rfletche@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/HiggsPhys/Run2/HGamma/xAOD/HGamTools/branches/HGamTools-00-00-51-branch/data/input/FlavorSys.txt ./InputFiles/FlavorSys_h015.txt
